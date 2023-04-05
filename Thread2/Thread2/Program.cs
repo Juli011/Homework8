b9bef5a38d669890925a68e129ff6ab6b1feb160
uuusss631@gmail.com <uuusss631@gmail.com>
2023-04-05 16:10:42 +0300

    create App

---
+++ Thread2/Thread2/Program.cs
@@ -0,0 +1,42 @@
+﻿int countDouble = 0;
+int countOdd = 0;
+
+object Double = new();
+object Odd = new();
+void generate(object obj)
+{
+    int a = 0;
+    Random rnd = new Random();
+    for (int i = 1; i<100; i++)
+    {
+        a = rnd.Next(1, 100);
+        if (a%2 == 0)
+            lock (Double) { countDouble++; }
+        else lock (Odd) { countOdd++; }
+
+    }
+}
+
+string[] tasks = new string[]
+{
+    "Task 1",
+    "Task 2",
+    "Task 3",
+    "Task 4",
+    "Task 5",
+    "Task 6",
+    "Task 7",
+    "Task 8",
+    "Task 9",
+    "Task 10"
+};
+
+foreach (string task in tasks)
+{
+    ThreadPool.QueueUserWorkItem(new WaitCallback(generate), task);
+}
+
+
+
+Console.WriteLine("countDouble - " + countDouble);
+Console.WriteLine("countOdd - " + countOdd);
